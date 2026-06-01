c550da0a81d149aab52435ddce679e0c130dd578
ismael haider <ismaelbanzer332211@gmail.com>
2026-06-01 23:21:32 +0300

    run project in main file and add session for details

--- Main.java
+++ Main.java
@@ -1,23 +1,10 @@
 package library_manage;
 
-import library_manage.controller.BookController;
-import library_manage.services.AuthorServices;
-import library_manage.services.BookServices;
-import library_manage.services.BorrowServices;
-import library_manage.services.UserServices;
+import javax.swing.SwingUtilities;
+import library_manage.view.LibraryFrame;
 
 public class Main {
     public static void main(String[] args) {
-        AuthorServices authorServices = new AuthorServices();
-        BookServices bookServices = new BookServices(authorServices);
-        UserServices userServices = new UserServices();
-        BorrowServices borrowServices = new BorrowServices(bookServices, userServices);
-        BookController bookController = new BookController(bookServices, authorServices);
-
-        System.out.println("Library data loaded from txt files.");
-        System.out.println("Authors: " + authorServices.getAllAuthors().size());
-        System.out.println("Books: " + bookServices.getBookCount());
-        System.out.println("Users: " + userServices.getUserCount());
-        System.out.println("Borrow records: " + borrowServices.getBorrowRecordCount());
+        SwingUtilities.invokeLater(() -> new LibraryFrame().setVisible(true));
     }
 }
